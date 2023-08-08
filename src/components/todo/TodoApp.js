fef57e55ec0aae1a391d12c1505f45ad563af3ea
JKKook <whdrnrdldia@naver.com>
2023-08-08 23:01:48 +0900

    FEAT: store lists in localStorage

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -1,29 +1,30 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import TodoItem from '../todo-item/TodoItem';
 import AddTodo from './AddTodo';
 
 export default function TodoApp() {
-    const [lists, setLists] = useState([
-        {
-            id: 1,
-            text: 'todos1',
-            status: 'active',
-        },
-        {
-            id: 2,
-            text: 'todos2',
-            status: 'active',
-        },
-    ]);
+    // 리스트 로컬에 저장
+    const [lists, setLists] = useState(
+        JSON.parse(localStorage.getItem('todoLists')) || [
+            { id: '1', text: 'todos1', status: 'active' },
+            { id: '2', text: 'todos2', status: 'active' },
+        ],
+    );
 
-    const handleAdd = (newList) => {
+    // 리스트의 상태 변화에 따른 로컬 저장
+    useEffect(() => {
+        localStorage.setItem('todoLists', JSON.stringify(lists));
+    }, [lists]);
+
+    const handleAddList = (newList) => {
         setLists([...lists, newList]);
     };
 
     return (
         <section>
-            <AddTodo onAdd={handleAdd} />
+            <AddTodo onAdd={handleAddList} />
             <ul>
                 {lists.map((list) => (
                     <TodoItem key={list.id} todo={list} />
