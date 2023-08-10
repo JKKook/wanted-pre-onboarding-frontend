4fbffdc2b74ccba69311a76373ea3ac1594a1124
JKKook <whdrnrdldia@naver.com>
2023-08-10 17:50:07 +0900

    FEAT: Add SignUp

--- src/components/sign/SignUp.js
+++ src/components/sign/SignUp.js
@@ -1,5 +1,82 @@
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
-    return <div></div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isValid, setIsValid] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleEmailChange = (e) => {
+        const inputEmail = e.target.value;
+        setEmail(inputEmail);
+        handleValidate(inputEmail, password);
+    };
+
+    const handlePasswordChange = (e) => {
+        const inputPassword = e.target.value;
+        setPassword(inputPassword);
+        handleValidate(email, inputPassword);
+    };
+
+    // 유효성 검사
+    const handleValidate = (newEmail, newPassword) => {
+        const isEmailValid = newEmail.includes('@');
+        const isPassword = newPassword.length >= 8;
+        setIsValid(isEmailValid && isPassword);
+    };
+
+    const handleSignSubmit = async () => {
+        if (isValid) {
+            try {
+                const response = await fetch(
+                    'https://www.pre-onboarding-selection-task.shop/auth/signup',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ email, password }),
+                    },
+                );
+
+                if (response.ok) {
+                    navigate('/signin');
+                } else {
+                    alert('Something wrong');
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    };
+
+    return (
+        <div>
+            <h1>회원가입 페이지</h1>
+            <input
+                data-testid='email-input'
+                type='email'
+                placeholder='이메일'
+                value={email}
+                onChange={handleEmailChange}
+            />
+            <input
+                data-testid='password-input'
+                type='password'
+                placeholder='비밀번호'
+                value={password}
+                onChange={handlePasswordChange}
+            />
+            <button
+                data-testid='signup-button'
+                disabled={!isValid}
+                onClick={handleSignSubmit}
+            >
+                회원가입
+            </button>
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import SignUp from './components/sign/SignUp';
@@ -12,21 +12,19 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
-        children: [
-            {
-                path: '/signup',
-                element: <SignUp />,
-            },
-            {
-                path: '/signin',
-                element: <SignIn />,
-            },
-            {
-                path: '/todo',
-                element: <TodoApp />,
-            },
-        ],
         errorElement: <NotFound />,
+    },
+    {
+        path: 'signup',
+        element: <SignUp />,
+    },
+    {
+        path: '/signin',
+        element: <SignIn />,
+    },
+    {
+        path: '/todo',
+        element: <TodoApp />,
     },
 ]);
 
@@ -34,5 +32,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <Outlet />
     </React.StrictMode>,
 );
